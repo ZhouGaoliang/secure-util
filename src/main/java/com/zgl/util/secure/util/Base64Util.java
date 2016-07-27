669d33c5c6196a30f609cdf3c0ab831b5a9a0625
906395223@qq.com <ZGL@DESKTOP-S31TB0E.vico-lab.com>
2016-07-27 16:19:46 +0800

    update to version 1.3

--- src/main/java/com/zgl/util/secure/util/Base64Util.java
+++ src/main/java/com/zgl/util/secure/util/Base64Util.java
@@ -1,5 +1,7 @@
 package com.zgl.util.secure.util;
 
+import java.io.UnsupportedEncodingException;
+
 import org.bouncycastle.util.encoders.Base64;
 
 public class Base64Util {
@@ -11,12 +13,28 @@ public class Base64Util {
 		return  Base64.decode(byteData);
 	}
 	
-	public static String encode(String data,String encoding) throws Exception{
+	public static String encode(String data,String encoding) throws UnsupportedEncodingException{
 		return new String(encode(data.getBytes(encoding)),encoding);
 	}
 	
-	public static String decode(String data,String encoding) throws Exception{
+	public static String decode(String data,String encoding) throws UnsupportedEncodingException{
 		return new String(decode(data.getBytes(encoding)),encoding);
+	}
+	
+	public static byte[] encode2Bytes(String data,String encoding) throws UnsupportedEncodingException{
+		return encode(data.getBytes(encoding));
+	}
+	
+	public static byte[] decode2Bytes(String data,String encoding) throws UnsupportedEncodingException{
+		return decode(data.getBytes(encoding));
+	}
+	
+	public static String encode2Str(byte[] byteData, String encoding) throws UnsupportedEncodingException{
+		return  new String(encode(byteData),encoding);
+	}
+	
+	public static String decode2Str(byte[] byteData,String encoding) throws UnsupportedEncodingException{
+		return new String(decode(byteData),encoding);
 	}
 	
 	
